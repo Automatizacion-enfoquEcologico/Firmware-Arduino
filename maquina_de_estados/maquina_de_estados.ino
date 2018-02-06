85bfd71881cfe5c9bd7d42214837d08b8eb9481d
Eduardo <eduardonunes.167@gmail.com>
2018-02-06 00:34:53 -0400

    mensaje eco serial

--- maquina_de_estados/maquina_de_estados.ino
+++ maquina_de_estados/maquina_de_estados.ino
@@ -1,5 +1,11 @@
 //  PINES
 
+#define LDR1 7
+#define LDR2 6
+#define HIGROMETRO 5
+#define CORRIENTE A0
+#define RELE 13
+
 //  ESTADOS
 
 #define STBY 0        //Se verifica el estado del bufer, se leen las entradas y se encapsula parte de la trama.
@@ -7,12 +13,33 @@
 #define ENVIO 3       //Se envia la trama al DemoQE
 #define SALIDAS 2     //Si hubo algun cambio en la salida reencapsula y actualizaq la salida (activa el riego).  
 
-int estado = 0;
+// ETIQUETAS PARA MASCARAS
 
-bool riego = false; 
+#define LDR
+#define H
+#define INICIO 255
+
+
+int estado = 0;
+byte data_in = 0;
+byte data_out = 0;
+byte adc = 0;
+
+bool riego = false;
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  pinMode (LDR1, INPUT_PULLUP);
+  pinMode (LDR2, INPUT_PULLUP);
+  pinMode (HIGROMETRO, INPUT_PULLUP);
+  pinMode (RELE, OUTPUT);
+
+  digitalWrite (RELE, riego);
+
 
 }
 
@@ -21,24 +48,40 @@ void loop() {
 
   switch (estado)
   {
-  case STBY:
-  
-  break;
-  case LECTURA:
-  
-  break;
-  case ENVIO:
-  
-  break;
-  
-  case SALIDAS:
-  
-  break;
-  default:
-  
-  break;
+    case STBY:
+      if (Serial.available() > 0) {
+        // read the incoming byte:
+        data_in = Serial.read();
+        //data_in = 0;
+        // say what you got:
+        Serial.write(data_in);
+        digitalWrite(RELE, riego);
+        riego = !riego;
 
-    
-    }
+        /*
+          if(riego)digitalWrite(RELE,HIGH);
+          else digitalWrite(RELE,LOW);
+          riego = !riego;
+          delay(50);
+        */
+      }
+
+      break;
+    case LECTURA:
+
+      break;
+    case ENVIO:
+
+      break;
+
+    case SALIDAS:
+
+      break;
+    default:
+
+      break;
+
+
+  }
 
 }
